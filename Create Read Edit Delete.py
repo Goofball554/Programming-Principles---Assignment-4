b796cf682ad1e7dc371d467ee4427e6d2ef6eedc
Alex <123321081+Goofball554@users.noreply.github.com>
2023-04-09 19:48:16 -0400

    Finished Code + Changed Read

--- Create Read Edit Delete.py
+++ Create Read Edit Delete.py
@@ -21,10 +21,10 @@ class UserInteraction:
             Read.Reading()
 
         elif userInput == 3:
-            Edit()
+            Edit.Editing()
 
         elif userInput == 4:
-            Delete()
+            Delete.Deleting()
 
         elif userInput == 5:
             file.close()
@@ -52,7 +52,7 @@ class Create:
                 if userInput2 > 0:
                     file.write(("\n" + str(userInput2) + " " + userInput1))
                     print("Successfully appended to the document!")
-                    print("Press 1 to return to the main menu, or anything else to close the program")
+                    print("Press 1 to return to the main menu, or anything else to close the program. ")
                     try:
                         userInput = int(input())
 
@@ -85,22 +85,29 @@ class Read:
     def Reading():
         try: 
             with open("Programming.txt", "r") as file:
-                print(file.read())           
-                print("Press 1 to return to the main menu, or anything else to close the program")
-            
-            try:
-                userInput = int(input())
+                search = input("Please enter either the book name or ID: ")
+                
+                lines = file.readlines()
+                
+                for line in lines:
+                    if line.find(search) != -1:
+                        print(line)
 
-            except:
-                userInput = 0
+                print("Press 1 to return to the main menu, or anything else to close the program.")
 
-            if userInput == 1:
-                file.close()
-                UserInteraction.PrintMainMenu()
+                try:
+                    userInput = int(input())
 
-            else:
-                file.close()
-        
+                except:
+                    userInput = 0
+
+                if userInput == 1:
+                    file.close()
+                    UserInteraction.PrintMainMenu()
+
+                else:
+                    file.close()
+
         except FileNotFoundError:
             print("File not found.")
 
@@ -112,11 +119,100 @@ class Read:
 
 class Edit:
     def Editing():
-        pass
+        try: 
+            try:
+                userInput1 = input("Please enter the ID followed by the name of the book you'd like to replace: ")
+                userInput2 = input("Please enter the ID followed by the name of the book you'd like to add: ")
+
+            except:
+                userInput1 = 0
+            
+            if userInput1 != 0:
+
+                with open("Programming.txt","r") as file:
+                    search = userInput1
+                    newline=[]
+
+                    for search in file.readlines():        
+                        newline.append(search.replace(userInput1, userInput2))
+
+                with open("Programming.txt","w") as file:
+                    for line in newline:
+                        file.writelines(line)
+                
+                print("Successfully edited!")
+
+                print("Press 1 to return to the main menu, or anything else to close the program.")
+
+                try:
+                    userInput = int(input())
+
+                except:
+                    userInput = 0
+
+                if userInput == 1:
+                    file.close()
+                    UserInteraction.PrintMainMenu()
+
+                else:
+                    file.close()
+
+            else:
+                print("What you entered is not valid.")
+                file.close()
+                Edit.Editing()
+
+        except FileNotFoundError:
+            print("File not found.")
+
+        except PermissionError:
+            print("Permission denied")
+
+        except Exception as e:
+            print(f"Error: {e}")
 
 class Delete:
     def Deleting():
-        pass
+        try: 
+            with open("Programming.txt", "r") as file:
+                lines = file.readlines()
+            
+            try:
+                userInput1 = int(input("Please enter the ID of the book you'd like to delete: "))
 
+            except:
+                userInput1 = 0
+
+            if userInput1 == 0:
+                print("What you entered is invalid.")
+                file.close()
+                Delete.Deleting()
+
+            else:
+                try:
+                    userInput2 = input("Please enter the name of the book you'd like to delete: ")
+
+                except:
+                    userInput2 = 0
+
+                if userInput2 == 0:
+                    print("What you entered is invalid.")
+                    file.close()
+                    Delete.Deleting()
+
+                else:
+                    with open("Programming.txt", "w") as file:
+                        for line in lines:
+                            if line.strip("\n") != (str(userInput1) + " " + userInput2):
+                                file.write(line)
+
+        except FileNotFoundError:
+            print("File not found.")
+
+        except PermissionError:
+            print("Permission denied")
+
+        except Exception as e:
+            print(f"Error: {e}")
 
 UserInteraction.Run()
