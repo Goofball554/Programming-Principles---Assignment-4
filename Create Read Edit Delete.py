a9af3657dbacb511cbde6cd0546a22ce9d48ecf4
Alex <123321081+Goofball554@users.noreply.github.com>
2023-04-10 17:30:39 -0400

    Added Comments

--- Create Read Edit Delete.py
+++ Create Read Edit Delete.py
@@ -1,7 +1,8 @@
+#This class handles the main menu interaction with the user
 class UserInteraction:
     def PrintMainMenu():
         try:
-            file = open("Programming.txt", "r")
+            #Prints the main menu and options for the user
             print("Please select one of the options:")
             print("1. Create")
             print("2. Read")
@@ -9,40 +10,54 @@ class UserInteraction:
             print("4. Delete")
             print("5. Close the Program")
 
+            #Asks the user for input
             userInput = int(input())
 
+        #User does not input / inputs something valid
         except:
             userInput = 0
 
+        #Takes the user to the Create menu
         if userInput == 1:
             Create.Creating()
 
+        #Takes the user to the Read (Search) menu
         elif userInput == 2:
             Read.Reading()
 
+        #Takes the user to the Edit menu
         elif userInput == 3:
             Edit.Editing()
 
+        #Takes the user to the Delete menu
         elif userInput == 4:
             Delete.Deleting()
 
+        #Ends the program
         elif userInput == 5:
-            file.close()
+            pass
 
+        #The user did not enter a valid input, reprints the main menu
         else:
             print("Please enter a valid input (1-5).")
             UserInteraction.PrintMainMenu()
 
+    #Runs the program
     def Run():
         UserInteraction.PrintMainMenu()
 
+#This class handles the creating functionality
 class Create:
     def Creating():
         try: 
+            #Opens the file in apend mode
             with open("Programming.txt", "a") as file:
+                
+                #Asks the user for input
                 userInput1 = input("Please enter the name of the book: ")    
                 print("Please enter the book's ID number: ")
                 
+                #Checking for valid input
                 try:
                     userInput2 = int(input())
 
@@ -50,9 +65,14 @@ class Create:
                     userInput2 = 0
 
                 if userInput2 > 0:
+                    #Appends the information to the file
                     file.write(("\n" + str(userInput2) + " " + userInput1))
                     print("Successfully appended to the document!")
+
+                    #Prompts the user to either return to the main menu or close the program
                     print("Press 1 to return to the main menu, or anything else to close the program. ")
+                    
+                    #Checking for valid input
                     try:
                         userInput = int(input())
 
@@ -66,11 +86,13 @@ class Create:
                     else:
                         file.close()
 
+                #Prompts the user that what they entered is not valid
                 else:
                     print("What you entered was not valid, please try again.")
                     file.close()
                     Create.Creating()
     
+        #Handling the various possible errors
         except FileNotFoundError:
             print("File not found.")
 
@@ -80,21 +102,27 @@ class Create:
         except Exception as e:
             print(f"Error: {e}")
     
-
+#This class handles the reading (searching) functionality
 class Read:
     def Reading():
         try: 
+            #Opens the file in read mode
             with open("Programming.txt", "r") as file:
+
+                #Promps the user to input their search terms
                 search = input("Please enter either the book name or ID: ")
                 
                 lines = file.readlines()
                 
+                #Searchs for the key terms in the file and prints if found
                 for line in lines:
                     if line.find(search) != -1:
                         print(line)
 
+                #Prompts the user to either return to the main menu or close the program
                 print("Press 1 to return to the main menu, or anything else to close the program.")
 
+                #Checking for valid input
                 try:
                     userInput = int(input())
 
@@ -108,6 +136,7 @@ class Read:
                 else:
                     file.close()
 
+        #Handling the various possible errors
         except FileNotFoundError:
             print("File not found.")
 
@@ -117,33 +146,43 @@ class Read:
         except Exception as e:
             print(f"Error: {e}")
 
+#This class handles the editing functionality
 class Edit:
     def Editing():
         try: 
             try:
-                userInput1 = input("Please enter the ID followed by the name of the book you'd like to replace: ")
-                userInput2 = input("Please enter the ID followed by the name of the book you'd like to add: ")
+                #Promps the user for the ID and book name they want to replace and what to replace it with
+                userInput1 = input("Please enter the ID the book you'd like to replace: ")
+                userInput2 = input("Please enter the name of the book you'd like to replace")
+                userInput3 = input("Please enter the name of the book you'd like to add: ")
 
             except:
                 userInput1 = 0
             
             if userInput1 != 0:
-
+                
+                #Opens the file in read mode
                 with open("Programming.txt","r") as file:
                     search = userInput1
                     newline=[]
 
+                    #Copies the contents of the file, swapping the two inputs
                     for search in file.readlines():        
-                        newline.append(search.replace(userInput1, userInput2))
+                        newline.append(search.replace(userInput2, userInput3))
 
+                #Opens the file in write mode
                 with open("Programming.txt","w") as file:
+
+                    #Rewrites the new text into the file
                     for line in newline:
                         file.writelines(line)
                 
                 print("Successfully edited!")
 
+                #Prompts the user to either return to the main menu or close the program
                 print("Press 1 to return to the main menu, or anything else to close the program.")
 
+                #Checking for valid input
                 try:
                     userInput = int(input())
 
@@ -157,11 +196,13 @@ class Edit:
                 else:
                     file.close()
 
+            #Promps the user that what they entered is not valid
             else:
                 print("What you entered is not valid.")
                 file.close()
                 Edit.Editing()
 
+        #Handling the various possible errors
         except FileNotFoundError:
             print("File not found.")
 
@@ -171,18 +212,22 @@ class Edit:
         except Exception as e:
             print(f"Error: {e}")
 
+#This class handles the deleting functionality
 class Delete:
     def Deleting():
         try: 
+            #Opens the program in read mode
             with open("Programming.txt", "r") as file:
                 lines = file.readlines()
             
             try:
+                #Asks the user for the ID of the book they'd like to delete
                 userInput1 = int(input("Please enter the ID of the book you'd like to delete: "))
 
             except:
                 userInput1 = 0
 
+            #Promps the user that what they entered is invalid
             if userInput1 == 0:
                 print("What you entered is invalid.")
                 file.close()
@@ -190,22 +235,21 @@ class Delete:
 
             else:
                 try:
+                    #Asks the user for the name of the book they'd like to delete
                     userInput2 = input("Please enter the name of the book you'd like to delete: ")
 
                 except:
                     userInput2 = 0
 
-                if userInput2 == 0:
-                    print("What you entered is invalid.")
-                    file.close()
-                    Delete.Deleting()
+                #Opens the file in write mode
+                with open("Programming.txt", "w") as file:
+                    for line in lines:
+                        
+                        #Removes the inputed text from the file
+                        if line.strip("\n") != (str(userInput1) + " " + userInput2):
+                            file.write(line)
 
-                else:
-                    with open("Programming.txt", "w") as file:
-                        for line in lines:
-                            if line.strip("\n") != (str(userInput1) + " " + userInput2):
-                                file.write(line)
-
+        #Handling the various possible errors
         except FileNotFoundError:
             print("File not found.")
 
@@ -215,4 +259,5 @@ class Delete:
         except Exception as e:
             print(f"Error: {e}")
 
+#Runs the program upon starting
 UserInteraction.Run()
